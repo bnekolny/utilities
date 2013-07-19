d975ad422f64a9bf1c394d841559281f88150089
Brett Nekolny <brett.nekolny@mapmyfitness.com>
2013-07-19 08:36:16 -0600

    Switching the to_gist scrip to use urllib2 instead of requests.

--- gist/to_gist.py
+++ gist/to_gist.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python
 
-import requests
+import urllib2
+import string
+import base64
 import json
 import sys
 import os
+
+# Request headers, will use basic auth so defining early
+headers = {}
  
-gist_url = 'https://api.github.com/gists'
+top_level_url = 'https://api.github.com'
+gist_url = '%s/gists' % top_level_url
 
 gh_user = os.environ.get('GITHUBUSERNAME')
 gh_pass = os.environ.get('GITHUBPASSWORD')
-gh_auth = (str(gh_user), str(gh_pass)) if gh_user and gh_pass else None
+# If there is a username and password, add it
+if gh_user and gh_pass:
+    auth = 'Basic ' + string.strip(base64.encodestring(gh_user + ':' + gh_pass))
+    headers['Authorization'] = auth
  
 input = []
 for line in sys.stdin:
@@ -35,15 +44,24 @@ content = {
     }
 }
  
+#sys.exit(0)
+headers['content-type'] = 'application/json'
+req = urllib2.Request(url=gist_url, data=json.dumps(content), headers=headers)
 
-r = requests.post(gist_url, auth=gh_auth, data=json.dumps(content),
-        headers={'content-type': 'application/json'})
+https_handler = urllib2.HTTPSHandler(debuglevel=0)
+opener = urllib2.build_opener(https_handler)
+urllib2.install_opener(opener)
+
+result = opener.open(req)
+status_code = result.getcode()
+content = result.read()
 
 # Check the response code from github, if good don't need to show
-if r.status_code not in [200, 201]:
-    print "Status Code: %s" % r.status_code
-    print "Content: %s" % json.dumps(content)
+if status_code not in [200, 201]:
+    print "Status Code: %s" % status_code
+    print "Content: %s" % content
     sys.exit(1)
 
+json_response = json.loads(content)
 # print the url
-print r.json().get('html_url')
+print json_response.get('html_url')
