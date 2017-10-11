6661d230dccd6e0d2816a1fe76c1935f358f38a4
Brett Nekolny <brett.nekolny@brandfolder.com>
2017-10-11 13:05:35 -0600

    Adding a script to create PRs on github.

--- bashrc_include.sh
+++ bashrc_include.sh
@@ -10,3 +10,22 @@ gist()
     echo -n $the_url | pbcopy
     echo "$the_url -- also on your clipboard"
 }
+
+createpr()
+{
+  branchname=$(git rev-parse --abbrev-ref HEAD)
+  commitsha=$(git rev-parse HEAD)
+  prname=$(git log --oneline -n 1 | cut -d ' ' -f 2-)
+
+  reponame=$(git remote -v | grep "github.com" | head -n 1 | cut -d ':' -f 2 | cut -d '.' -f 1)
+  echo "creating PR $prname on $reponame, master...$branchname:$commitsha"
+
+  curl -s -X POST "https://api.github.com/repos/$reponame/pulls?access_token=$GITHUB_TOKEN" -d @- <<EOF | grep "html_url" | head -n 1 | cut -d '"' -f 4 | xargs open
+{
+  "title": "$prname",
+  "body": "cli generated PR",
+  "head": "$branchname",
+  "base": "master"
+}
+EOF
+}
