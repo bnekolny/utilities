2a790aaf3785755bc47921e5e7eafe523504166a
Brett Nekolny <brett.nekolny@brandfolder.com>
2018-04-04 14:36:30 -0600

    Adding a function to change to a recent branch

--- bashrc_include.sh
+++ bashrc_include.sh
@@ -29,3 +29,14 @@ createpr()
 }
 EOF
 }
+
+myfunctochangebranches()
+{
+  PS3='Please choose which branch to jump to: '
+  IFS=$'\n'; options=( $(git show-recent-branches) ); 
+  select opt in "${options[@]}"
+  do
+    git checkout $opt
+    break
+  done
+}
